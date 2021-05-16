bca03106ba57350a4636c48dfc121ca98f1887cd
Ivo Jawer <ivojawerbaum@gmail.com>
2021-05-16 16:52:14 -0300

    first commit

---
+++ pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "native-base";
+import React from "react";
+
+export function Home (){
+
+    return (
+        <View>
+            <Text>Wollok Mobile!</Text>
+        </View>
+    )
+
+}
