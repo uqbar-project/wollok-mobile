c271b51d707ede8a2529f1af08074f172c35d521
Ivo Jawer <ivojawerbaum@gmail.com>
2021-05-18 18:47:01 -0300

    feat: ui improvements - definition type

--- components/Definition.tsx
+++ components/Definition.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { useTheme } from "@react-navigation/native";
-import { Container, Item, ListItem, Text, View } from "native-base";
+import { ListItem, Text } from "native-base";
+import { Image } from 'react-native';
 
-export function Definition(props: {name: string}) {
+export type Definition = {
+    name: string,
+    difinitionType: 'object' | 'class' 
+}
+
+export function DefinitionComponent(props: {definition: Definition}) {
     const {colors} = useTheme()
     return (
-        <View style={{
-            marginHorizontal: 5,
+        <ListItem   noIndent={true} style={{
             padding: 10,
             marginTop: 6,
             backgroundColor: colors.notification,
-            borderRadius: 20
         }}>
-            <Text>{props.name}</Text>
-        </View>
+            <Image source={getImageFromType(props.definition.difinitionType)} style={{marginRight: 10}}></Image>
+            <Text>{props.definition.name}</Text>
+        </ListItem>
     )
+}
+
+function getImageFromType(aType: string){
+    switch(aType){
+        case "class": 
+            return require('../assets/class.png')
+        case "object":
+            return require('../assets/wko.png')
+    }
 }
--- pages/Definitions.tsx
+++ pages/Definitions.tsx
@@ -1,13 +1,15 @@
 import { useTheme } from "@react-navigation/native";
 import { Fab, Header, Icon, List, Text, View } from "native-base";
 import React from "react";
-import { Definition } from "../components/Definition";
+import { DefinitionComponent, Definition } from "../components/Definition";
 
 export function Definitions (){
     const theme = useTheme()
     return (
         <View style={{flex: 1}}>
-            {getDefinitions().map(def => <Definition key={def.name} name={def.name}></Definition>)}
+            <List>
+                {getDefinitions().map(def => <DefinitionComponent key={def.name} definition={def}></DefinitionComponent>)}
+            </List>
 
             <Fab style={{backgroundColor: theme.colors.primary}}>
                 <Icon name='add'></Icon>                
@@ -16,9 +18,10 @@ export function Definitions (){
     )
 }
 
-function getDefinitions(){
+function getDefinitions(): Definition[]{
     return [
-        {name: 'pepita'},
-        {name: 'alpiste'}
+        {name: 'pepita', difinitionType:'object'},
+        {name: 'alpiste', difinitionType:'object'},
+        {name: 'Entrenador', difinitionType: 'class'}
     ]
 }
