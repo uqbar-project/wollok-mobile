458d90db0050e73ba3fab3a94a8ef2cdf013b062
Ivo Jawer <ivojawerbaum@gmail.com>
2021-05-25 02:57:41 -0300

    feat: setup multilanguage

--- App.tsx
+++ App.tsx
@@ -1,39 +1,65 @@
-import React from "react";
-import { Entities } from "./pages/Entities/Entities";
-import { theme } from "./theme";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import React from "react"
+import { Entities } from "./pages/Entities/Entities"
+import { theme } from "./theme"
+import { createStackNavigator } from "@react-navigation/stack"
+import { NavigationContainer } from "@react-navigation/native"
 import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper'
+import { setI18nConfig, translate } from "./utils/translation-helpers"
+import RNLocalize from 'react-native-localize'
+import { upperCaseFirst } from 'upper-case-first'
 
+export default class App extends React.Component {
 
-const App = () => {
-   const Stack = createStackNavigator();
-
-   const paperTheme: ReactNativePaper.Theme = {
-      ...DefaultTheme,
-      colors: {
-         ...DefaultTheme.colors,
-         ...theme.colors,
-         accent: theme.colors.border
-      }
+   constructor(props: {} | Readonly<{}>) {
+      super(props)
+      setI18nConfig()
    }
-   return (
-      <PaperProvider theme={paperTheme}>
-         <NavigationContainer theme={theme}>
-            <Stack.Navigator>
-               <Stack.Screen
-                  name="entities"
-                  component={Entities}
-                  options={{ title: 'Entidades', headerTitleAlign: 'center' }}
-               ></Stack.Screen>
-            </Stack.Navigator>
-         </NavigationContainer>
-      </PaperProvider>
-   )
-};
+
+   componentDidMount() {
+      RNLocalize.addEventListener("change", this.handleLocalizationChange)
+   }
+
+   componentWillUnmount() {
+      RNLocalize.removeEventListener("change", this.handleLocalizationChange)
+   }
+
+   handleLocalizationChange = () => {
+      setI18nConfig()
+      this.forceUpdate()
+   }
+
+
+
+   render() {
+      const Stack = createStackNavigator()
+      const paperTheme: ReactNativePaper.Theme = {
+         ...DefaultTheme,
+         colors: {
+            ...DefaultTheme.colors,
+            ...theme.colors,
+            accent: theme.colors.border
+         },
+         dark: theme.dark
+      }
+      return (
+         <PaperProvider theme={paperTheme}>
+            <NavigationContainer theme={theme}>
+               <Stack.Navigator screenOptions={{ headerStyle }}>
+                  <Stack.Screen
+                     name="entities"
+                     component={Entities}
+                     options={{ title: upperCaseFirst(translate("entities")), headerTitleAlign: 'center' }}
+                  ></Stack.Screen>
+               </Stack.Navigator>
+            </NavigationContainer>
+         </PaperProvider>
+      )
+   }
+}
+
+const headerStyle = { elevation: 0, shadowOpacity: 0 }
 
 
 
 
 
-export default App;
