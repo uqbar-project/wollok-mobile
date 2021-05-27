221f46e4207bd67e9b721a8fdb20e521f35559de
Ivo Jawer <ivojawerbaum@gmail.com>
2021-05-26 21:41:30 -0300

    fix: prevent app from crashing on language change

--- android/app/src/main/java/com/wollokmobile/MainActivity.java
+++ android/app/src/main/java/com/wollokmobile/MainActivity.java
@@ -1,9 +1,32 @@
 package com.wollokmobile;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactInstanceManager;
 
 public class MainActivity extends ReactActivity {
 
+  static String currentLocale;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      MainActivity.currentLocale = getResources().getConfiguration().locale.toString();
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+      super.onConfigurationChanged(newConfig);
+
+      String locale = newConfig.locale.toString();
+      if (!MainActivity.currentLocale.equals(locale)) {
+          MainActivity.currentLocale = locale;
+          final ReactInstanceManager instanceManager = getReactInstanceManager();
+          instanceManager.recreateReactContextInBackground();
+      }
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
