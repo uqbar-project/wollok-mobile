982e8f6e2bee1c8abc6816df64776b6421fcfd69
ivojawer <ivojawerbaum@gmail.com>
2021-06-10 00:30:16 -0300

    wip

--- pages/EntityDetails/EntityDetails.tsx
+++ pages/EntityDetails/EntityDetails.tsx
@@ -1,15 +1,33 @@
-import { Text } from 'react-native-paper'
-import React from "react";
+import { Text, List, Divider } from 'react-native-paper'
+import React, { useState } from "react";
 import { FabAddScreen } from "../../components/FabScreen";
-import { RouteProp } from '@react-navigation/native';
+import { RouteProp, useRoute } from '@react-navigation/native';
 import { RootStackParamList } from '../../App';
 
 
 
-export function EntityDetails(props: {route: RouteProp<RootStackParamList, 'EntityDetails'>}){
-    return(
+export function EntityDetails(props: { route: RouteProp<RootStackParamList, 'EntityDetails'> }) {
+    console.log(props.route.params.entity.methods())
+    return (
         <FabAddScreen onPress={() => console.log('hola')}>
-            <Text>{props.route.params.entity.name}</Text>
+            <List.Section>
+                <List.Accordion
+                    title="ATRIBUTOS"
+                > 
+                    <List.Item title="First item" />
+                    <Divider style={{ marginHorizontal: 10, backgroundColor: 'white' }} />
+                    <List.Item title="Second item" />
+                </List.Accordion> 
+
+
+                <List.Accordion
+                    title="METODOS"
+                > 
+                    <List.Item title="First item" />
+                    <Divider style={{ marginHorizontal: 10, backgroundColor: 'white' }} />
+                    <List.Item title="Second item" />
+                </List.Accordion> 
+            </List.Section>
         </FabAddScreen>
     )
 }
