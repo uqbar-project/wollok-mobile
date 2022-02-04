fa7a76d90bc1adade8861b337fffb8c4ee2a213d
ivo <ivojawerbaum@gmail.com>
2022-02-04 20:02:13 -0300

    only stop on weakref in android

--- src/weak-ref/WeakRef.ts
+++ src/weak-ref/WeakRef.ts
@@ -1,13 +1,15 @@
+import { Platform } from 'react-native'
+
 /* eslint no-undef: "off" */
 export class WeakRef<T> {
-	constructor(private object: T) {
-		console.log(`weakref: ${object}`)
-	}
+	constructor(private object: T) {}
 
 	deref() {
 		return this.object
 	}
 }
 
-//@ts-ignore
-globalThis.WeakRef = WeakRef
+if (Platform.OS === 'android') {
+	//@ts-ignore
+	globalThis.WeakRef = WeakRef
+}
