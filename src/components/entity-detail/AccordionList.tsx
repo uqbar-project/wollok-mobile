9e164cb972a73701645da27208e16dbf3064856b
LAPTOP-REU1DSQU\naguv <naguvaris@gmail.com>
2022-02-06 13:36:40 -0300

    feat(entityDetails): accordion list starts opened

--- src/components/entity-detail/AccordionList.tsx
+++ src/components/entity-detail/AccordionList.tsx
@@ -1,5 +1,5 @@
 import { Theme, useTheme } from '@react-navigation/native'
-import React, { Key } from 'react'
+import React, { Key, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Divider, List as ListComponent } from 'react-native-paper'
 import { List } from 'wollok-ts/dist/model'
@@ -8,14 +8,22 @@ type Props<Item> = {
 	title: string
 	items: Item[]
 	VisualItem: React.FC<{ item: Item }>
+	initialExpanded?: boolean
 }
 export const AccordionList = function <
 	Item extends { name: Key; parameters?: List<any> },
 >(props: Props<Item>) {
 	const reactNavigationTheme = useTheme()
 	const styles = getStyles(reactNavigationTheme)
+	const [expanded, setExpanded] = useState<boolean>(
+		props.initialExpanded || false,
+	)
+
 	return (
-		<ListComponent.Accordion title={props.title}>
+		<ListComponent.Accordion
+			title={`${props.title} (${props.items.length})`}
+			expanded={expanded}
+			onPress={() => switchExpanded()}>
 			{props.items.map(item => {
 				return (
 					<View key={`${item.name}${item.parameters?.length}`}>
@@ -26,6 +34,10 @@ export const AccordionList = function <
 			})}
 		</ListComponent.Accordion>
 	)
+
+	function switchExpanded() {
+		setExpanded(!expanded)
+	}
 }
 
 function getStyles(theme: Theme) {
