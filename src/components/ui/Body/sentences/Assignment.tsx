14d93306aa50d4f7058d5fa555aabf14de7d8577
ivo <ivojawerbaum@gmail.com>
2022-04-22 18:10:24 -0300

    assignment refactor

--- src/components/ui/Body/sentences/Assignment.tsx
+++ src/components/ui/Body/sentences/Assignment.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import { IconButton } from 'react-native-paper'
 import { Assignment as AssignmentModel } from 'wollok-ts/dist/model'
 import { ReferenceSegment } from '../../../expressions/expression-segment'
-import {
-	display,
-	ExpressionDisplay,
-} from '../../../expressions/ExpressionDisplay'
+import { display } from '../../../expressions/ExpressionDisplay'
 import { Row } from '../../Row'
+import { ReferenceExpression } from './ReferenceExpression'
 
 export const Assignment = ({ assignment }: { assignment: AssignmentModel }) => {
 	return (
 		<Row style={display}>
 			<ReferenceSegment text={assignment.variable.name} index={0} />
-			<IconButton icon="arrow-right" />
-			<ExpressionDisplay expression={assignment.value} withIcon={false} />
+			<ReferenceExpression expression={assignment.value} />
 		</Row>
 	)
 }
--- src/components/ui/Body/sentences/Variable.tsx
+++ src/components/ui/Body/sentences/Variable.tsx
@@ -3,31 +3,22 @@ import { IconButton, Text } from 'react-native-paper'
 import { Variable as VariableModel } from 'wollok-ts/dist/model'
 import { useTheme } from '../../../../theme'
 import { isNullExpression } from '../../../../utils/wollok-helpers'
-import { ExpressionDisplay } from '../../../expressions/ExpressionDisplay'
+import { ConstantVariableIcon } from '../../ConstantVariableIcon'
 import { Row } from '../../Row'
+import { ReferenceExpression } from './ReferenceExpression'
 
 export const Variable = (props: { variable: VariableModel }) => {
 	const theme = useTheme()
 	return (
 		<Row>
 			<IconButton icon="variable" color={theme.colors.primary} />
-			{props.variable.isConstant ? (
-				<IconButton icon="lock" color={theme.colors.primary} />
-			) : (
-				<></>
-			)}
+			<ConstantVariableIcon variable={props.variable} />
 			<Text>{props.variable.name}</Text>
-			{!isNullExpression(props.variable.value) ? (
-				<>
-					<IconButton icon="arrow-right" />
-					<ExpressionDisplay
-						expression={props.variable.value}
-						withIcon={false}
-					/>
-				</>
-			) : (
-				<></>
-			)}
+			<>
+				{!isNullExpression(props.variable.value) && (
+					<ReferenceExpression expression={props.variable.value} />
+				)}
+			</>
 		</Row>
 	)
 }
