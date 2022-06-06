69183043c02b90af1ced7b8af54a1bda8dae9e87
palumbon <nahuel.palumbo@gmail.com>
2022-06-06 14:55:23 +0200

    Filter Wollok objects from locals

--- src/components/debugging/LocalsInspector.tsx
+++ src/components/debugging/LocalsInspector.tsx
@@ -9,28 +9,31 @@ function LocalsInspector() {
 		<ScrollView>
 			{interpreter.evaluation.currentFrame.contextHierarchy().map(context => (
 				<View key={context.id}>
-					{[...context.locals.keys()].map(local => {
-						const value = context.get(local)
-						const stringValue = interpreter
-							.fork()
-							//TODO: Move to context
-							.do(function* () {
-								return value && (yield* this.send('toString', value))
-							})
-							.finish()
+					{[...context.locals.keys()]
+						.filter(local => !['true', 'false', 'null'].includes(local))
+						.filter(local => !local.startsWith('wollok'))
+						.map(local => {
+							const value = context.get(local)
+							const stringValue = interpreter
+								.fork()
+								//TODO: Move to context
+								.do(function* () {
+									return value && (yield* this.send('toString', value))
+								})
+								.finish()
 
-						const valueLabel = stringValue.error
-							? 'ERROR!'
-							: stringValue.result?.innerValue ?? 'null'
+							const valueLabel = stringValue.error
+								? 'ERROR!'
+								: stringValue.result?.innerValue ?? 'null'
 
-						return (
-							<List.Item
-								key={`${local}-${valueLabel}`}
-								title={local}
-								description={valueLabel}
-							/>
-						)
-					})}
+							return (
+								<List.Item
+									key={`${local}-${valueLabel}`}
+									title={local}
+									description={valueLabel}
+								/>
+							)
+						})}
 				</View>
 			))}
 		</ScrollView>
